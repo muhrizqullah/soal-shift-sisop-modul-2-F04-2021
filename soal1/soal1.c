8db09988032067e74532356489050e7306cb9d68
Misdinar <55637655+Misdinar@users.noreply.github.com>
2021-04-14 13:17:27 +0700

    Update soal1.c
    ada tambahan fungsi.... njir bingung biar waitnya bisa nunggu sesuai donlotnya :(

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -9,12 +9,9 @@
 
 char path[20];
 
-void download(){
-  char pathdir[] ="/home/misdinar/Sisop1/modul2/no1/";
-  time_t t_now = time(NULL);
-  struct tm waktu = *localtime(&t_now);
+void download(){  
   pid_t child_id;
-  int i,status;
+  int i,status,status1,status2;
   child_id = fork();
 
   if (child_id < 0) {
@@ -28,9 +25,13 @@ void download(){
     {"/bin/wget", "wget", "--no-check-certificate", "https://drive.google.com/uc?id=1FsrAzb9B5ixooGUs0dGiBr-rC7TS9wTD&export=download", "-O", "Foto_for_Stevany.zip", 0},
     {"/bin/wget", "wget", "--no-check-certificate", "https://drive.google.com/uc?id=1ktjGgDkL0nNpY-vT7rT7O6ZI47Ke9xcp&export=download", "-O", "Film_for_Stevany.zip", 0}};
     for(i = 0; i< 3;i++ ){
-      if (0 == fork()) continue;
+      if (0 == fork()){
+        while ((wait(&status1)) > 0);
+        continue;
+      } 
+      while ((wait(&status2)) > 0);
       execve(argv[i][0], &argv[i][0], NULL);
-      while ((wait(&status)) > 0);
+     
      }
 
      char *argv_1[3][5] = {
@@ -45,12 +46,31 @@ void download(){
    else{
     // this is parent
     while ((wait(&status)) > 0);
+    while ((wait(&status1)) > 0);
     char *argv_3[] = {"unzip", "*.zip", NULL};
     execv("/usr/bin/unzip", argv_3);
 
+  }
+}
+
+void hbd(){
+  char pathdir[] ="/home/misdinar/Sisop1/modul2/no1/";
+  time_t t_now = time(NULL);
+  struct tm waktu = *localtime(&t_now);
+
+  pid_t child_id;
+  child_id = fork();
+
+  if (child_id < 0) {
+    exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
+  } 
+
+  if (child_id == 0) {
+    strftime(path, sizeof(path)-1, "%d-%m_%H:%M", &waktu);
+
     while(strcmp(path,"09-04_22:22")!=0)
     {
-    sleep(1);
+    sleep(2);
     t_now = time(NULL);
     waktu = *localtime(&t_now);
     strftime(path, sizeof(path)-1, "%d-%m_%H:%M", &waktu);
@@ -78,4 +98,7 @@ int main() {
     printf("%s_%d\n", path, strcmp(path,"09-04_16:22"));
   }
   download();
+  puts("selesai ekstrak");
+  hbd();
+  return 0;
 }

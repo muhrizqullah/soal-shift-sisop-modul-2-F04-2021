e178c0a64d5cec62047c5e0a99b4ff25c60dafa4
Misdinar <55637655+Misdinar@users.noreply.github.com>
2021-04-24 03:21:06 +0800

    FIX Revisi
    YEAYY KELARRR

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -14,7 +14,6 @@
 #include <glob.h>
 
 char path[20];
-short flag=0;
 
 void filter_prosess(char* sumber, char* tujuan)
 {
@@ -35,7 +34,6 @@ void filter_prosess(char* sumber, char* tujuan)
     globbuf_film.gl_pathv[0] = "cp";
     globbuf_film.gl_pathv[1] = "-r";
     execvp("cp", &globbuf_film.gl_pathv[0]);
-    exit(1);
     }
 }
 
@@ -56,10 +54,6 @@ while(wait(&status2)>0);
   pid_t child_id;
   child_id = fork();
 
-  if (child_id < 0) {
-    exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
-  } 
-
   if (child_id == 0) {
     char *argv_6[] = {"rm","-rf", "MUSIK", "FOTO", "FILM", NULL};
     execv("/bin/rm", argv_6);
@@ -67,11 +61,10 @@ while(wait(&status2)>0);
     while(wait(&status)>0);
     char *argv_4[] = {"zip", "-rm", "Lopyu_Stevany", ".", "-x", "soal1*", "*.zip", NULL};
     execv("/bin/zip", argv_4);
-    flag=0;
   }
 }
 
-void wget_img(char* link, char* name)
+void wget_zip(char* link, char* name)
 {
     pid_t child_id;
     int status;
@@ -85,68 +78,66 @@ void wget_img(char* link, char* name)
     {
       char *argv[] = {"wget", link, "-q", "-O", name, NULL};
       execv("/usr/bin/wget", argv);
-      exit(1);
+    }
+}
+
+void unzip()
+{
+    pid_t child_id;
+    int status;
+
+    child_id = fork();
+
+    if (child_id < 0)
+        exit(EXIT_FAILURE);
+
+    if (child_id == 0)
+    {
+      char *argv_3[] = {"unzip", "*.zip", NULL};
+      execv("/usr/bin/unzip", argv_3); 
+    }
+}
+
+void folder()
+{
+    pid_t child_id;
+    int status;
+
+    child_id = fork();
+
+    if (child_id < 0)
+        exit(EXIT_FAILURE);
+
+    if (child_id == 0)
+    {
+      char *argv_1[] = {"mkdir", "-p", "Musyik", "Fylm", "Pyoto", NULL};
+      execv("/bin/mkdir", argv_1);
     }
 }
 
 void download(){  
-  pid_t child_id;
+
   int i,j,status,status1,status2,status3,status4,status5,status6;
-  child_id = fork();
 
-  if (child_id < 0) {
-    exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
-  } 
-
-  if (child_id == 0) {
-    // this is child
     char *argv[3][2] = {    
     {"https://drive.google.com/uc?id=1ZG8nRBRPquhYXq_sISdsVcXx5VdEgi-J&export=download", "Musik_for_Stevany.zip"},
     {"https://drive.google.com/uc?id=1FsrAzb9B5ixooGUs0dGiBr-rC7TS9wTD&export=download", "Foto_for_Stevany.zip"},
     {"https://drive.google.com/uc?id=1ktjGgDkL0nNpY-vT7rT7O6ZI47Ke9xcp&export=download", "Film_for_Stevany.zip"}};
     for(i = 0; i< 3;i++ ){
-      wget_img(argv[i][0], argv[i][1]);
-      }
+      wget_zip(argv[i][0], argv[i][1]);
     }
-   else{
-    // this is parent
-    // sleep(40);
+
     while ((wait(&status2)) > 0);
-    // kill(child_id, SIGKILL); 
-    // while ((wait(&status3)) > 0);
-    pid_t child_id1;
-    child_id1 = fork();
-    
-  if (child_id1 < 0) {
-    exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
-  } 
+    sleep(3);
 
-  if (child_id1 == 0) {
-    char *argv_3[] = {"unzip", "*.zip", NULL};
-    execv("/usr/bin/unzip", argv_3); 
-  }else{
-    while ((wait(&status4)) > 0);
-    id_t child_id2;
-    child_id2 = fork();
+    unzip();
 
-  if (child_id2 < 0) {
-    exit(EXIT_FAILURE);
-  }  
+    while ((wait(&status3)) > 0);
 
-  if(child_id2==0){
-    while ((wait(&status)) > 0);
-    char *argv_1[] = {"mkdir", "-p", "Musyik", "Fylm", "Pyoto", NULL};
-    execv("/bin/mkdir", argv_1);
-  }else{
+    folder();
+
       while ((wait(&status5)) > 0);
-      id_t child_id3;
-      child_id3 = fork();
-
-      if (child_id3 < 0) {
-      exit(EXIT_FAILURE);
-      }  
-
-      if(child_id3==0){
+ 
       char *argv_filter[3][2] = {    
       {"FILM/*.mp4", "Fylm/"},
       {"FOTO/*.jpg", "Pyoto/"},
@@ -155,16 +146,11 @@ void download(){
       filter_prosess(argv_filter[i][0], argv_filter[i][1]);
       }
       
-      }else{
          while ((wait(&status6)) > 0);
         hbd();
-        // return;
-      } 
-    }    
-    }
-}
 
-}
+      } 
+
 int main() {
   pid_t pid, sid;
   pid = fork();
@@ -188,10 +174,20 @@ int main() {
   while(1){
   time (&my_time);
   timeinfo = localtime (&my_time);
-  if(timeinfo->tm_mday == 9 && timeinfo->tm_mon+1 == 4 && timeinfo->tm_hour == 16 && timeinfo->tm_min == 22 && flag==0){
-    download();
-    flag=1;
+  if(timeinfo->tm_mday == 9 && timeinfo->tm_mon+1 == 4 && timeinfo->tm_hour == 16 && timeinfo->tm_min == 21 && timeinfo->tm_sec == 59){
+     pid_t child_id;
+        child_id = fork();
+
+        if (child_id < 0)
+        {
+            exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
+        }
+
+        if (child_id == 0)
+        {
+            download();
+        }
+        sleep(3);
   }
-  sleep(2);
   }
 }
